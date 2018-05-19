f72757de57bd2a8836eda5b1101a2202b0371956
izqcool <584942555@qq.com>
2018-05-19 19:21:33 +0800

    add time helper

--- src/components/Helpers/Helpers.js
+++ src/components/Helpers/Helpers.js
@@ -9,8 +9,35 @@ export class Helpers {
         const nowTamp = Date.parse(now)/1000;
         const distanceTime = oldTamp - nowTamp;
 
-        if(distanceTime>=0 && distanceTime <= 60) {
-            return `${distanceTime}秒前`;
+        if(distanceTime>=0 && distanceTime < 60) {
+            return `${distanceTime} seconds age`;
+        }
+        if(distanceTime>=60 && distanceTime < 3600 ) {
+            const min = Math.floor(distanceTime/60);
+            if(min>1) {
+                return `${min} minutes age`;
+            }else {
+                return `${min} minute age`;
+            }
+
+        }
+
+        if(distanceTime>=3600 && distanceTime < 3600*24 ) {
+            const hour = Math.floor(distanceTime/3600);
+            if(hour>1) {
+                return `${hour} hours age`;
+            }else {
+                return `${hour} hour age`;
+            }
+        }
+
+        if(distanceTime>=3600*24 ) {
+            const day = Math.floor(distanceTime/3600*24);
+            if(day>1) {
+                return `${day} days age`;
+            }else {
+                return `${day} day age`;
+            }
         }
 
     }
