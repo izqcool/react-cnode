b323bb51d0fd3ed849ae2c2c0be0db72ed03a3eb
izqcool <584942555@qq.com>
2018-05-19 22:53:42 +0800

    fix: fix time error

--- src/components/Helpers/Helpers.js
+++ src/components/Helpers/Helpers.js
@@ -4,10 +4,10 @@ export class Helpers {
     static dateFromNow(strDate) {
         const strTime = strDate;
         //以秒为单位的时间戳
-        const oldTamp = Date.parse(new Date(stringTime))/1000;
+        const oldTamp = Date.parse(new Date(strTime))/1000;
         const now = new Date();
         const nowTamp = Date.parse(now)/1000;
-        const distanceTime = oldTamp - nowTamp;
+        const distanceTime = nowTamp - oldTamp;
 
         if(distanceTime>=0 && distanceTime < 60) {
             return `${distanceTime} seconds age`;
@@ -32,7 +32,7 @@ export class Helpers {
         }
 
         if(distanceTime>=3600*24 ) {
-            const day = Math.floor(distanceTime/3600*24);
+            const day = Math.floor(distanceTime/(3600*24));
             if(day>1) {
                 return `${day} days age`;
             }else {
