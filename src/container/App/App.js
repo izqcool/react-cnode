3912c64de55681c408575193109fff3e05c6356a
izqcool <584942555@qq.com>
2018-05-22 15:17:02 +0800

    solve the router problem

--- src/container/App/App.js
+++ src/container/App/App.js
@@ -2,27 +2,27 @@ import React from 'react';
 import {Route, Switch} from 'react-router-dom';
 import {Header} from '../Header';
 import {Home} from '../Home';
-import 'normalize.css';
 import * as styles from './App.scss';
 
 export class App extends React.Component {
 
     render() {
         const {history,location} = this.props;
-        console.log(history);
-        console.log(location);
 
         return (
             <div className={styles.app} >
                 <Header location={location}/>
                 <div className={styles.content}>
-                    <Route exact path="/" component={ ()=>(<Home tab="all"/>)} />
-                    <Route exact path="/home" component={ ()=>(<Home tab="all"/>)} />
-                    <Route exact path="/good" component={ ()=>(<Home tab="good"/>)} />
-                    <Route exact path="/ask" component={ ()=>(<Home tab="ask"/>)} />
-                    <Route exact path="/share" component={ ()=>(<Home tab="share"/>)} />
-                    <Route exact path="/job" component={ ()=>(<Home tab="job"/>)} />
+                    <Switch>
+                        <Route exact path="/" component={ ()=>(<Home tab="all" history={history}/>)} />
+                        <Route exact path="/home" component={ ()=>(<Home tab="all" history={history}/>)} />
+                        <Route exact path="/good" component={ ()=>(<Home tab="good" history={history}/>)} />
+                        <Route exact path="/ask" component={ ()=>(<Home tab="ask" history={history}/>)} />
+                        <Route exact path="/share" component={ ()=>(<Home tab="share" history={history}/>)} />
+                        <Route exact path="/job" component={ ()=>(<Home tab="job" history={history}/>)} />
+                    </Switch>
                 </div>
+
             </div>
         )
     }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import 'normalize.css';
 import ReactDom from 'react-dom';
 import {HashRouter as Router, Route, Switch} from 'react-router-dom';
-import {App} from './container';
+import {App,Article} from './container';
 
 
 // console.log(process.env.NODE_ENV);
@@ -9,7 +10,13 @@ global.__DEVELOPMENT__ = process.env.NODE_ENV === 'development';
 const vDomTree = (
     <Router>
         <Switch>
-            <Route path="/" component={App} />
+            <Route exact path="/" component={App} />
+            <Route exact path="/home" component={App} />
+            <Route exact path="/good" component={App} />
+            <Route exact path="/ask" component={App} />
+            <Route exact path="/share" component={App} />
+            <Route exact path="/job" component={App} />
+            <Route exact path="/topic/:id"  component={Article}/>
         </Switch>
     </Router>
 
