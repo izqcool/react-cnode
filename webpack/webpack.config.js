58779dabb16f8896ef705ec7afc55fa5593f64c3
izqcool <584942555@qq.com>
2018-05-24 15:43:25 +0800

    add font-awesome support

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -89,6 +89,9 @@ module.exports = {
                     }
 
                 ]
+            },
+            {
+                test: /\.(gif|jpg|png|woff|svg|eot|ttf)\??.*$/, loader: 'url-loader?limit=50000&name=[path][name].[ext]'
             }
         ]
     },
