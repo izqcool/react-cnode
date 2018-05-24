b61d3472dd427cb0f0b35f87c2b1d1d8b1a25053
izqcool <584942555@qq.com>
2018-05-24 17:56:33 +0800

    feat(view): add user page

---
+++ src/container/User/User.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import * as styles from './User.scss';
+
+export class User extends React.Component {
+
+    render() {
+        return (
+            <div className={styles.user}>
+
+            </div>
+        )
+    }
+}
