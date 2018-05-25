033ff2476c616ce4e1628819253621e80f06d5ae
izqcool <584942555@qq.com>
2018-05-25 15:47:19 +0800

    feat(event): get user info

--- src/container/User/User.js
+++ src/container/User/User.js
@@ -1,13 +1,50 @@
 import React from 'react';
+import {Http,Loading,UserHeader} from '../../components';
 import * as styles from './User.scss';
+
+const http = new Http();
 
 export class User extends React.Component {
 
-    render() {
-        return (
-            <div className={styles.user}>
+    constructor(props) {
+        super(props);
+        this.state = {
+            dataLoaded: false
+        };
+        this.getUserInfo = this.getUserInfo.bind(this);
+    };
 
-            </div>
-        )
+    componentDidMount() {
+        this.getUserInfo();
     }
+
+    getUserInfo() {
+        const {match} = this.props;
+        const name = match.params.username;
+        http.get(`/user/${name}`).then((res) => {
+            console.log(res);
+            this.setState({
+                data: res.data,
+                dataLoaded: true
+            });
+        }).catch((err) => {
+            console.log(err);
+        })
+    }
+
+    render() {
+        const {dataLoaded,data} = this.state;
+        if(dataLoaded) {
+            return (
+                <div className={styles.user}>
+                    <UserHeader/>
+                </div>
+            )
+        }else {
+            return (
+                <Loading/>
+            )
+        }
+    }
+
 }
