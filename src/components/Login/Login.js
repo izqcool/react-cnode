25a5b2252f5c06432dd94e9ca3ba7c5a6b482df0
izqcool <584942555@qq.com>
2018-05-29 18:05:18 +0800

    add token event

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {Http} from '../../components';
 import * as styles from './Login.scss';
+
+const http = new Http();
 
 export class Login extends React.Component {
 
@@ -18,6 +21,7 @@ export class Login extends React.Component {
         super(props);
         this.inputRef = React.createRef();
         this.onCancel = this.onCancel.bind(this);
+        this.onConfirm = this.onConfirm.bind(this);
     }
 
     componentDidMount() {
@@ -28,6 +32,20 @@ export class Login extends React.Component {
         this.props.onCancel();
     }
 
+    onConfirm() {
+        const accessToken = this.inputRef.current.value;
+        console.log(accessToken);
+        http.post(`/accesstoken`,{
+            params: {
+                "accesstoken": accessToken
+            }
+        }).then((res)=>{
+            console.log(res);
+        }).catch((err)=>{
+            console.log(err);
+        });
+    }
+
     render() {
         return (
             <div className={styles.login}>
@@ -36,7 +54,7 @@ export class Login extends React.Component {
                 </div>
                 <div className={styles.content}>
                     <input type="text" placeholder="请输入Access Token" ref={this.inputRef}/>
-                    <button>登 录</button>
+                    <button onClick={this.onConfirm}>登 录</button>
                 </div>
             </div>
         )
--- src/components/index.js
+++ src/components/index.js
@@ -5,3 +5,4 @@ export * from './Loading';
 export * from './ArticleHeader';
 export * from './ToolButton';
 export * from './UserHeader';
+export * from './Login';
