ad70ddbbd96b9ea8ac22541334cb814084102099
izqcool <584942555@qq.com>
2018-05-31 16:31:47 +0800

    add

--- src/container/Comment/Comment.js
+++ src/container/Comment/Comment.js
@@ -1,12 +1,48 @@
 import React from 'react';
 import * as styles from './Comment.scss';
+import {Storage} from '../../classes';
 
 export class Comment extends React.Component {
 
+    constructor(props) {
+        super(props);
+        this.goBack = this.goBack.bind(this);
+    }
+
+    componentDidMount() {
+        const repliesData = Storage.get('replies');
+        console.log(repliesData);
+    }
+
+    goBack() {
+        window.history.go(-1);
+    }
+
+
+
     render() {
+        const repliesData = Storage.get('replies');
         return (
-            <div>
-                this is comment page
+            <div className={styles.container}>
+                <div className={styles.header}>
+                    <div className={styles.back} onClick={this.goBack}>
+                        返回
+                    </div>
+                    <div className={styles.comment}>
+                        评论
+                        {
+                            repliesData ? (
+                                `  ${repliesData.length}`
+                            ) : (null)
+                        }
+                    </div>
+                </div>
+                <div className={styles.content}>
+
+                </div>
+                <div className={styles.footer}>
+
+                </div>
             </div>
         )
     }
