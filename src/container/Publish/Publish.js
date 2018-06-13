645aa8a00d94180abe60b99aba95b5a01a905b3d
izqcool <584942555@qq.com>
2018-06-13 17:05:16 +0800

    add publish page

--- src/container/Publish/Publish.js
+++ src/container/Publish/Publish.js
@@ -5,8 +5,12 @@ export class Publish extends React.Component {
 
     render() {
         return (
-            <div>
-                this is public page
+            <div className={styles.container}>
+                <div className={styles.header}>
+                    <div className={styles.back}>返回</div>
+                    <div className={styles.title}>发表帖子</div>
+                    <div className={styles.publish}>发布</div>
+                </div>
             </div>
         )
     }
