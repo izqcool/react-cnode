c6bef5d3aa7f59ce89edcfe8080f68100c53e29f
izqcool <584942555@qq.com>
2018-06-21 15:53:03 +0800

    add setDialog

--- src/components/UserHeader/UserHeader.js
+++ src/components/UserHeader/UserHeader.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {Helpers} from '../../classes';
+import {Helpers,SmartDialog} from '../../classes';
 import * as styles from './UserHeader.scss';
 
 export class UserHeader extends React.Component {
+
+    setDialog = null;
 
     static propTypes = {
         avatarUrl: PropTypes.string.isRequired,
@@ -25,6 +27,8 @@ export class UserHeader extends React.Component {
         super(props);
         this.goGithub = this.goGithub.bind(this);
         this.onGoPublish = this.onGoPublish.bind(this);
+        this.onShowSet = this.onShowSet.bind(this);
+        this.onSignOut = this.onSignOut.bind(this);
     }
 
     goGithub(){
@@ -35,6 +39,46 @@ export class UserHeader extends React.Component {
     onGoPublish() {
         const {history} = this.props;
         history.push(`/publish`);
+    }
+
+    onSignOut() {
+        const {history} = this.props;
+        window.localStorage.removeItem('cnodeLoginName');
+        window.localStorage.removeItem('cnodeId');
+        window.localStorage.removeItem('cnodeUrl');
+        window.localStorage.removeItem('cnodeToken');
+        this.setDialog.close();
+        history.push(`/`);
+    }
+
+    onShowSet() {
+        this.setDialog = new SmartDialog({
+            styles: {},
+            maskStyles: {
+                backgroundColor:'#7d7c7c'
+            },
+            allowOutsideClick: false,
+            component: (
+                <div className={styles.setDialog}>
+                    <div className={styles.header}>
+                        <i className="fa fa-cog" aria-hidden="true"></i>
+                        <div>设置</div>
+                    </div>
+                    <div className={styles.about}>
+                        <i className="fa fa-eye" aria-hidden="true"></i>
+                        <div>关于</div>
+                    </div>
+                    <div className={styles.out} onClick={this.onSignOut}>
+                        <i className="fa fa-sign-out" aria-hidden="true"></i>
+                        <div>退出</div>
+                    </div>
+                    <div className={styles.close}>
+                        <i className="fa fa-times-circle-o" aria-hidden="true" onClick={()=>{this.setDialog.close()}}></i>
+                    </div>
+                </div>
+            )
+        });
+        this.setDialog.show();
     }
 
     render() {
@@ -58,7 +102,7 @@ export class UserHeader extends React.Component {
                         </div>
                         {
                             isSelf && (
-                                <div className={styles.set}>
+                                <div className={styles.set} onClick={this.onShowSet}>
                                     <i className="fa fa-cog" aria-hidden="true"></i>
                                 </div>
                             )
